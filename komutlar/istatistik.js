c06c38cf3ad2877810fa528eccb47b3dc4eb04fe
Glitch (reddawn-v12-guardbotu) <none>
2021-03-01 07:11:43 +0000

    🏦🎺 Checkpoint ./komutlar/istatistik.js:18825712/10497 ./events/message.js:18825712/45

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,31 +1,38 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"); 
+const client = new Discord.Client();
+const moment = require("moment");
+require("moment-duration-format");
 
+module.exports.run = async(client, message, args) => {
+  const duration = moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]");
 
-exports.run = async (client, message, args) => {
-
-  
-const istatistikler = new Discord.MessageEmbed()
-  .addField(`:scroll: »  Pingim` ,`${client.ws.ping}ms`,true)
-  .addField(`:incoming_envelope: » Yapımcım` ,`<@609093740157599796>`,true)
-  .addField(`:label: » Node.js`, `${process.version}`, true)
- .addField(`:bar_chart: » Kanal Sayısı`, `${client.channels.cache.size  }`, true)
- .addField(`:postbox: » Kullanıcı Sayısı`, `${client.users.cache.size}`, true)
- .addField(`:envelope: » Sunucu Sayısı`, `${client.guilds.cache.size}`, true)
- .addField(`» Linkler`, `[Destek Sunucusu](https://discord.gg) | [Youtube](https://www.youtube.com/`, true)
-  message.channel.send(istatistikler)
-  
-  
+    const embed = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setTitle(':bar_chart: İstatistik;')
+    .addField(':timer: Gecikme: ', client.ping + 'ms')
+      .addField(':headphones: Müzik Çalınan Sunucu Sayısı;', client.voiceConnections.size)
+    .addField(':construction_worker: Çalışma Süresi: ', `${duration}`)
+    .addField(':busts_in_silhouette: Kullanıcılar:', client.guilds.reduce((a, b) => a + b.memberCount, 0))
+    .addField(':tv: Kanallar:', client.channels.size)
+    .addField(':clipboard: Sunucular:', client.guilds.size)
+    .addField(':desktop: Bellek kullanımı:', (process.memoryUsage().heapUsed / 2048 / 2048).toFixed(2))
+    .addField(':book: Kütüphanesi;', `Discord.js`)
+    .addField(`Discord.js sürümü:`, Discord.version)
+      .addField(':spy: Yapımcım:', '<@IDNİZ> \n `CodeMareFi`')
+      .addField(':paperclip: Botun Başlanma Zamanı:', "Bot **22.10.2019**'de yapılmaya başlanmıştır")
+    .setFooter('CodeMareFi', client.user.avatarURL)
+    .setTimestamp()
+    message.channel.send(embed);
 }
-  
 exports.conf = {
   enabled: true,
-  guildOnly: false,
-  aliases: ['istatistik'],
+    guildOnly: true,
+  aliases: ['istatistik', 'botbilgi', 'bot-bilgi'],
   permLevel: 0
 };
 
 exports.help = {
-  name: 'i',
-  description: 'Otorol sistemini ayarlamaya yarar.',
-  usage: '-otorol-ayarla @rol #kanal'
-}; 
+  name: 'bilgi-bot',
+  description: 'İstediğiniz şeyi bota yazdırır.',
+  usage: 'istatistik [bot durumunu yazar]'
+};
